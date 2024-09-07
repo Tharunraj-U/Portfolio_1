b995ff37a19aa1f05a8db4db298eb9600cc172ab
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2024-09-08 02:22:35 +0530

    1464. Maximum Product of Two Elements in an Array

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { styles } from "../styles";
 import { services } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn, textVariant } from "../Utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import emailjs from "@emailjs/browser";
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import SectionWrapper from '../hoc/SectionWrapper';
-import { slideIn } from "../utils/motion";
+import { slideIn } from "../Utils/motion";
 
 const Contact = () => {
   const formRef = useRef();
